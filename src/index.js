f75b2dd77e85b50acf27e5310deeddd492fdd9ee
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-05-23 20:06:25 -0300

    json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, resp) => {
-    resp.send('Bem Vindo Ao , Express');
+    resp.json({message: 'Bem Vindo Ao , Express'});
 })
 
 app.listen(3434);
